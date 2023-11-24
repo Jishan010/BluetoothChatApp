7bdc3daa9736966dfe7810ae5117608b94aa90d2
Jishan Ansari <jishan010@gmail.com>
2023-11-24 16:10:43 +0530

    If applied,this commit will setup location and bluetooth permission settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.jishan.bluetoothchatapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jishan.bluetoothchatapp"
--- app/src/main/java/com/jishan/bluetoothchatapp/MainActivity.kt
+++ app/src/main/java/com/jishan/bluetoothchatapp/MainActivity.kt
@@ -1,8 +1,21 @@
 package com.jishan.bluetoothchatapp
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,30 +23,227 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.jishan.bluetoothchatapp.ui.theme.BluetoothChatAppTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<Array<String>>
+    private lateinit var enableBluetoothLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        locationPermissionSetup()
+        bluetoothSetup()
+
         setContent {
             BluetoothChatAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Jishan")
                 }
             }
         }
     }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private val receiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    val deviceName = if (ActivityCompat.checkSelfPermission(
+                            context, Manifest.permission.BLUETOOTH_CONNECT
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return
+                    } else {
+                        device?.name
+                    }
+                    val deviceHardwareAddress = device?.address // MAC address
+                    deviceName?.let {
+                        Log.e("Bluetooth deviceName", it + " Jishan Ansari hu main")
+                    }
+
+                    deviceHardwareAddress?.let {
+                        Log.e("Bluetooth deviceHardwareAddress", it + " Jishan Ansari hu main")
+                    }
+
+                    Log.e("Bluetooth deviceName", deviceName ?: "Kuch ni he behnchod")
+                    Log.e(
+                        "Bluetooth deviceHardwareAddress",
+                        deviceHardwareAddress ?: "Kuch ni he behnchod"
+                    )
+
+                }
+            }
+        }
+    }
+
+
+    private fun bluetoothSetup() {
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        enableBluetoothLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    // Bluetooth has been enabled
+                    Toast.makeText(this, "Bluetooth enabled", Toast.LENGTH_SHORT).show()
+                } else {
+                    // User did not enable Bluetooth or an error occurred
+                    Toast.makeText(this, "Bluetooth not enabled", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Toast.makeText(this, "Device doesn't support Bluetooth", Toast.LENGTH_SHORT).show()
+        }
+
+        // Check if Bluetooth is enabled or not, and if not, try to enable it
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            enableBluetoothLauncher.launch(enableBtIntent)
+        }
+
+        if (bluetoothAdapter?.isEnabled == true) {
+            // Start discovery
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_SCAN
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            bluetoothAdapter.startDiscovery()
+            Log.e("Bluetooth", "Discovery started")
+        }
+
+        // Register for broadcasts when a device is discovered.
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, filter)
+    }
+
+    private fun locationPermissionSetup() {
+
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                Log.d("Permission Result", permissions.toString())
+                permissions.entries.forEach { entry ->
+                    when (entry.key) {
+
+                        Manifest.permission.ACCESS_FINE_LOCATION -> {
+                            if (entry.value) {
+                                // Background location permission granted
+                                Log.e("Background location", entry.value.toString())
+                            } else {
+                                // Background location permission denied
+                            }
+                        }
+
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION -> {
+                            if (entry.value) {
+                                // Background location permission granted
+                                Log.e("Background location", entry.value.toString())
+                            } else {
+                                // Background location permission denied
+                            }
+                        }
+
+                        Manifest.permission.BLUETOOTH_CONNECT -> {
+                            if (entry.value) {
+                                // Bluetooth connect permission granted
+                                Log.e("BLUETOOTH_CONNECT", entry.value.toString())
+                            } else {
+                                // Bluetooth connect permission denied
+                            }
+                        }
+
+                        Manifest.permission.BLUETOOTH_SCAN -> {
+                            if (entry.value) {
+                                // Bluetooth connect permission granted
+                                Log.e("BLUETOOTH_SCAN", entry.value.toString())
+                            } else {
+                                // Bluetooth connect permission denied
+                            }
+                        }
+                    }
+                }
+            }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val permissionsToRequest = mutableListOf<String>()
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionsToRequest.add(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionsToRequest.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionsToRequest.add(Manifest.permission.BLUETOOTH_CONNECT)
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.BLUETOOTH_SCAN
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionsToRequest.add(Manifest.permission.BLUETOOTH_SCAN)
+            }
+
+
+
+            if (permissionsToRequest.isNotEmpty()) {
+                requestPermissionLauncher.launch(permissionsToRequest.toTypedArray())
+            }
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver)
+    }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
